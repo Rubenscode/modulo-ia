60d79de4c0f8dd6a5faeac455db48c95ec404e5d
rubenscode <rubens.andrade1@gmail.com>
2025-11-10 21:23:09 -0300

    Calcula media simples das notas de um aluno

---
+++ atividade-02/media_escolar.py
@@ -0,0 +1,16 @@
+# Calculadora de Média Escolar
+
+# Notas do Aluno
+nota_1 = float(input("Digite a primeira nota: "))
+nota_2 = float(input("Digite a segunda nota: "))
+nota_3 = float(input("Digite a terceira nota: "))
+
+# Calculo da Media
+media = (nota_1 + nota_2 + nota_3) / 3
+
+# Exibição dos Resultados
+print(f"Notas do Aluno")
+print(f"Nota 1: {nota_1}")
+print(f"Nota 2: {nota_2}")
+print(f"Nota 3: {nota_3}")
+print(f"Media Final: {media:.2f}")
