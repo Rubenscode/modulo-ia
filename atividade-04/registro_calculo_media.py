70b844b87ef35f51b3bfd6190f3b6d8ade76c4a3
rubenscode <rubens.andrade1@gmail.com>
2025-11-11 21:19:46 -0300

    Calculo de media

---
+++ atividade-04/registro_calculo_media.py
@@ -0,0 +1,24 @@
+def registrar_notas():
+    notas = []
+    while True:
+        try:
+            entrada = input("Digite a nota do aluno para continuar ou fim para encerrar: ")
+            if entrada.lower() == 'fim':
+                break
+            
+            nota = float(entrada)
+
+            if nota >= 0 and nota <= 10:
+                notas.append(nota)
+            else:
+                print("Nota invalida. Digite um valor entre 0 e 10")
+                continue
+        except ValueError:
+            print("Entrada Invallida. Por favor Digite somente numeros, Digite uma nota de 0 a 10 ou fim para encerrar")
+
+    if notas: 
+        media = sum(notas) / len(notas)
+        print(f"A media da turma: {media:.2f}")
+        print(f"Total de notas validas registradas: {len(notas)}")
+
+registrar_notas()
