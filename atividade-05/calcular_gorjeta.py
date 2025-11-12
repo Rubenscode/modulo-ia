74f2c7e865061b0b933b389ff7d6c7442216c2a1
rubenscode <rubens.andrade1@gmail.com>
2025-11-12 19:56:43 -0300

    Calcular gorjeta a ser paga

---
+++ atividade-05/calcular_gorjeta.py
@@ -0,0 +1,8 @@
+def caclular_gorjeta(valor_conta: float, porcentagem_gorjeta: float) -> float:
+    gorjeta = valor_conta * (porcentagem_gorjeta / 100)
+    return gorjeta
+
+total_conta = float(input("Digite o total da sua conta do Restaurante: "))
+porcentagem = float(input("Quantos % você deseja pagar de gorjeta: "))
+gorjeta = caclular_gorjeta(total_conta, porcentagem)
+print(f"Para uma conta de R${total_conta:.2f}, voce deseja pagar {porcentagem:.2f}% de gorjeta = R${gorjeta:.2f}")
