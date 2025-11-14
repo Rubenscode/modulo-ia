7aab51b9b2425deba9d0ae0c3ca74c2021d8be35
rubenscode <rubens.andrade1@gmail.com>
2025-11-14 19:32:12 -0300

    Criar arquivos CSV

---
+++ atividade-07/escrever_csv.py
@@ -0,0 +1,15 @@
+import csv
+
+def criar_csv(nome_arquivo):
+    dados = [
+        ['Ana', 28, 'Rio de Janeiro'],
+        ['Pedro', 34, "Sao Paulo"],
+        ['Kiane', 200, "Salvador"]
+    ]
+    with open(nome_arquivo, 'w', newline='', encoding='utf-8') as arquivo_csv:
+        escritor = csv.writer(arquivo_csv)
+        escritor.writerow(['Nome', 'Idade', 'Cidade'])
+        for linha in dados:
+            escritor.writerow(linha)
+
+criar_csv("dados_teste.csv")
